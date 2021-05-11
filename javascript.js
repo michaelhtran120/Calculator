f07129083f2f79741eb3b5dd48aba1b06d511820
michaelhtran120 <michaelhtran120@gmail.com>
2021-05-11 12:35:19 -0700

    update css, js and html

--- javascript.js
+++ javascript.js
@@ -30,6 +30,7 @@ const numberButton = document.querySelectorAll('.number-button')
 //Variable to store value
 let valueDisplay = '';
 let valueString = [];
+// Variable to store mathematical function.
 let calculate;
 
 function updateDisplay(valueDisplay) {
@@ -38,16 +39,10 @@ function updateDisplay(valueDisplay) {
 
 numberButton.forEach((button) => {
     button.addEventListener('click', () => {
-        if (!valueDisplay == '' && !calculate == '') {
-            clearDisplay();
-            valueDisplay += button.textContent;
-            updateDisplay(valueDisplay);
-        } else {
-            valueDisplay += button.textContent;
-            updateDisplay(valueDisplay);
-        }
+        valueDisplay += button.textContent;
+        updateDisplay(valueDisplay);
     });
-});
+});  
 
 // Function to clear
 const clearButton = document.getElementById('clear-button');
@@ -65,20 +60,103 @@ clearButton.addEventListener('click', () => {
 // Declaring variable for mathematical functions.
 const additionButton = document.getElementById('addition-button');
 const subtractionButton = document.getElementById('subtraction-button');
-const mulitplicationButton = document.getElementById('multiplication-button');
+const multiplicationButton = document.getElementById('multiplication-button');
 const divisionButton = document.getElementById('division-button');
 const equalButton = document.getElementById('equal-button');
 
-// Event Listener to get calculator to work.
+    // Event Listener to get calculator to work.
 additionButton.addEventListener('click', () => {
-    valueString.push(parseInt(valueDisplay));
-    calculate = addition;
+    if (valueDisplay === '') {
+        calculate = addition;
+        decimalButton.disabled = false;
+    } else {
+        valueString.push(parseFloat(valueDisplay));
+        valueDisplay = '';
+        calculate = addition;
+        decimalButton.disabled = false;
+    }
+})
+
+subtractionButton.addEventListener('click', () => {
+    if (valueDisplay === '') {
+        calculate = subtraction;
+        decimalButton.disabled = false;
+    } else {
+        valueString.push(parseFloat(valueDisplay));
+        valueDisplay = '';
+        calculate = subtraction;
+        decimalButton.disabled = false;
+    }
+})
+
+divisionButton.addEventListener('click', () => {
+    if (valueDisplay === '') {
+        calculate = division;
+        decimalButton.disabled = false;
+    } else {
+        valueString.push(parseFloat(valueDisplay));
+        valueDisplay = '';
+        calculate = division;
+        decimalButton.disabled = false;
+    }
+})
+
+multiplicationButton.addEventListener('click', () => {
+    if (valueDisplay === '') {
+        calculate = multiplication;
+        decimalButton.disabled = false;
+    } else {
+        valueString.push(parseFloat(valueDisplay));
+        valueDisplay = '';
+        calculate = multiplication;
+        decimalButton.disabled = false;
+    }
 })
 
 equalButton.addEventListener('click', () => {
-    valueString.push(parseInt(valueDisplay));
-    valueDisplay = operate(valueString, calculate);
-    updateDisplay(valueDisplay);
-    valueString = [];
-    calculate = '';
+    if (valueDisplay == 0 && calculate == division) {
+        valueDisplay = 'ERROR!';
+        updateDisplay(valueDisplay);
+    } else if (valueDisplay == '' || calculate == '') {
+        valueString = [];
+        valueDisplay = '';
+        calculate = ''
+    } else if (decimalCheck(valueDisplay) === true) {
+        valueString.push(parseFloat(valueDisplay));
+        valueDisplay = operate(valueString, calculate);
+        updateDisplay(valueDisplay.toFixed(2));
+        valueString = [valueDisplay];
+        valueDisplay = '';
+        calculate = '';
+        decimalButton.disabled = false;
+    } else {
+        valueString.push(parseFloat(valueDisplay));
+        valueDisplay = operate(valueString, calculate);
+        updateDisplay(valueDisplay);
+        valueString = [valueDisplay];
+        valueDisplay = '';
+        calculate = '';
+        decimalButton.disabled = false;
+    }
+})
+
+//Function to test if number contains decimal => if decimal, toFixed(2).
+function decimalCheck(num) {
+    if ((num - Math.floor(num)) !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Decimal point button and event listener.
+const decimalButton = document.getElementById('decimal-button');
+
+decimalButton.addEventListener('click', () => {
+    if (valueDisplay.includes('.')) {
+        decimalButton.disabled = true;
+    } else {
+        valueDisplay += decimalButton.textContent;
+        updateDisplay(valueDisplay);
+    }
 })
